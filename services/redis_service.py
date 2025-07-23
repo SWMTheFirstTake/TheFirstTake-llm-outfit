bc8d5ddc878a629190541f9c79b116f8889eb7e9
Hyeonseong0917 <sagiri0917@gmail.com>
2025-07-23 19:32:29 +0900

    redis config 수정

--- services/redis_service.py
+++ services/redis_service.py
@@ -19,7 +19,7 @@ class RedisService:
             self.redis_client = redis.Redis(
                 host=os.getenv("REDIS_HOST", "localhost"),
                 port=int(os.getenv("REDIS_PORT", "6379")),
-                db=int(os.getenv("REDIS_DB", "0")),
+                # db=int(os.getenv("REDIS_DB", "0")),
                 decode_responses=True,
                 socket_connect_timeout=5,
                 socket_timeout=5
